6f2c394ab66d1d9ae234cd73d63dfc4ada6bb63e
lasselian <lassesorensen80@gmail.com>
2026-03-12 20:38:08 +0100

    1.4
    oops

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtGui import QDesktopServices
 from PyQt6.QtCore import QUrl
 
-VERSION = "1.3.2"
+VERSION = "1.4"
 
 def _create_task_safe(coro):
     """Schedule an async task safely from synchronous Qt context.
